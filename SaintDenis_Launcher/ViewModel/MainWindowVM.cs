c36fbc77fcd5423489540b82f8fdfcb3b47cd69a
Thomas Bastin <thomasbastin366@gmail.com>
2024-02-13 03:17:48 +0100

    :art: Add Status Info + Version Info

--- SaintDenis_Launcher/ViewModel/MainWindowVM.cs
+++ SaintDenis_Launcher/ViewModel/MainWindowVM.cs
@@ -1,6 +1,7 @@
 ﻿using OokiiTsuki.Palette;
 using System.ComponentModel;
 using System.Printing.IndexedProperties;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -12,7 +13,8 @@ namespace SaintDenis_Launcher.ViewModel
     {
         #region Properties
         private Color _backgroundColor;
-        private BitmapImage _backgroundImage;
+        private BitmapImage? _backgroundImage;
+        private String _currentVersion;
         #endregion
 
         #region Accessors
@@ -21,19 +23,30 @@ namespace SaintDenis_Launcher.ViewModel
             get { return _backgroundColor; }
             set { _backgroundColor = value; OnPropertyChanged(); }
         }
-        public BitmapImage BackgroundImage
+        public BitmapImage? BackgroundImage
         {
             get { return _backgroundImage; }
             set { _backgroundImage = value; OnPropertyChanged(); }
+        }
+
+        public String CurrentVersion
+        {
+            get { return _currentVersion; }
+            set { _currentVersion = value; OnPropertyChanged(); }
         }
         #endregion
 
         #region Constructors
         public MainWindowVM()
         {
-            BackgroundImage = new BitmapImage(new Uri(@".Resources/Background_Chinois.png", UriKind.Relative));
+            CurrentVersion = "Unkown";
+
+            BackgroundImage = new BitmapImage(new Uri(@".Resources/Chinois_Background.png", UriKind.Relative));
             Palette palette = Palette.Generate(BackgroundImage);
-            BackgroundColor = palette.GetDarkMutedColor();
+            BackgroundColor = palette.GetDarkMutedColor(Colors.Black);
+            Version version = Assembly.GetExecutingAssembly()
+                                      .GetName().Version!;
+            CurrentVersion = version.ToString();
         }
         #endregion
 
